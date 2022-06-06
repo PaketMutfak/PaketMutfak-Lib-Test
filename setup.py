50ff440711d6fadd07db27bd44a1d817856316d6
emreozgun <emre.ozgun@paketmuftak.com.tr>
2022-06-06 11:19:17 +0300

    First test commit !

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='paketmutfak-tool-box',
+    version='0.0.3',
+    author='Paket Mutfak',
+    author_email='dev@paketmutfak.com.tr',
+    description='Testing installation of Package',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/PaketMutfak/PaketMutfak-Lib-Test',
+    project_urls={
+        "Bug Tracker": "https://github.com/PaketMutfak/PaketMutfak-Lib-Test/issues"
+    },
+    license='MIT',
+    packages=['tools'],
+    install_requires=['requests'],
+)
