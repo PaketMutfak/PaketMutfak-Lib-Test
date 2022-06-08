df94952649dbcb7395d884f8c0f261508835a4e6
nurullahgundogdu <nurullah.gundogdu@paketmutfak.com.tr>
2022-06-08 14:29:34 +0300

    slack mail işlemlerinin eklenmesi #1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name='paketmutfak',
-    version='1.0.6',
+    version='1.0.7',
     author='Paket Mutfak',
     author_email='dev@paketmutfak.com.tr',
     description='Testing installation of Package',
@@ -24,7 +24,8 @@ setup(
                       'datetime',
                       'iso8601',
                       'slugify',
-                      'PyJWT'],
+                      'PyJWT',
+                      'beartype'],
 )
 
 
