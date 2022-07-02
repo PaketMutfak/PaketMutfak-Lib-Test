0be688e404bbf258be177d6c9492ba3b18f5ff63
nurullahgundogdu <nurullah.gundogdu@paketmutfak.com.tr>
2022-07-02 18:10:24 +0300

    tablo isimleri düzenlendi #1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name='paketmutfak',
-    version='1.0.23',
+    version='1.0.24',
     author='Paket Mutfak',
     author_email='dev@paketmutfak.com.tr',
     description='Testing installation of Package',
