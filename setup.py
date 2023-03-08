6eb1b599cb94dc7f806066a15543b57343359540
Yasin Beyazlı <89048757+ysnbyzli@users.noreply.github.com>
2023-03-08 11:20:51 +0300

    fix: new log system

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name='paketmutfak',
-    version='0.2.2',
+    version='0.2.3',
     author='Paket Mutfak',
     author_email='dev@paketmutfak.com.tr',
     description='Testing installation of Package',
