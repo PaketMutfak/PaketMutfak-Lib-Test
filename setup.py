2efff44effdd741fcccfc892e61bb0f401dbc650
Yasin Beyazlı <89048757+ysnbyzli@users.noreply.github.com>
2023-03-17 13:55:48 +0300

    fix(version): change version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name='paketmutfak',
-    version='1.0.55',
+    version='1.0.56',
     author='Paket Mutfak',
     author_email='dev@paketmutfak.com.tr',
     description='Testing installation of Package',
