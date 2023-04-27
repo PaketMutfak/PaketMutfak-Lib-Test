bae78c5436cf3c2fcd07081b1fcca797065acdb2
Yasin Beyazlı <89048757+ysnbyzli@users.noreply.github.com>
2023-04-27 14:11:44 +0300

    feat: added platform products and optio class

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import find_packages, setup
 
-__version__ = "1.1.15"
+__version__ = "1.1.16"
 
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
