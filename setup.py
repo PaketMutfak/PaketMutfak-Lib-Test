d5c87556c8800f8a1819bd6b2bc1a108ad559e75
Yasin Beyazlı <89048757+ysnbyzli@users.noreply.github.com>
2023-05-10 14:35:07 +0300

    fix: error messages
    [sc-4496]

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import find_packages, setup
 
-__version__ = "1.1.17"
+__version__ = "1.1.18"
 
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
