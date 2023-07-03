6d6685f01fa92625f472958ea48e0257b0720d6a
Yasin Beyazlı <89048757+ysnbyzli@users.noreply.github.com>
2023-07-03 20:25:43 +0300

    fix: version

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import find_packages, setup
 
-__version__ = "1.1.38"
+__version__ = "1.1.39"
 
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
